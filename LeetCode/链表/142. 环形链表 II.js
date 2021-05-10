088a9e023fb29f8f53e76b6dceeb5a3150bd39b8
adminthw <154589317@qq.com>
2021-05-10 22:40:47 +0800

    update

---
+++ LeetCode/链表/142. 环形链表 II.js
@@ -0,0 +1,58 @@
+/* 
+最简单的就是给把每个都存入hashmap中，
+遇到重复的返回就好
+*/
+
+var detectCycle = function (head) {
+  let map = new Set();
+  while (head) {
+    if (!map.has(head)) {
+      map.add(head);
+    } else {
+      return head;
+    }
+    head = head.next;
+  }
+  return null;
+};
+
+/* 
+题目要求空间复杂度为o(1)！！！
+
+环形链表1使用的是判圈算法！
+其实快慢指针之间存在关系----，设链表中环外部分的长度为 a。slow 指针进入环后，又走了 b 的距离与 fast 相遇。
+此时，fast 指针已经走完了环的 n 圈，
+因此它（fast）走过的总距离为 a+n(b+c)+b=a+(n+1)b+nc
+任意时刻，fast 指针走过的距离都为 slow 指针的 2 倍。因此，我们有
+
+a+(n+1)b+nc=2(a+b) => a=c+(n-1)(b+c)
+
+从相遇点到入环点的距离加上n-1圈的环长，恰好等于从链表头部到入环点的距离
+
+
+
+
+*/
+var detectCycle = function(head) {
+  if (head === null) {
+      return null;
+  }
+  let slow = head, fast = head;
+  while (fast !== null) {
+      slow = slow.next;
+      if (fast.next !== null) {
+          fast = fast.next.next;
+      } else {
+          return null;
+      }
+      if (fast === slow) {
+          let ptr = head;
+          while (ptr !== slow) {
+              ptr = ptr.next;
+              slow = slow.next;
+          }
+          return ptr;
+      }
+  }
+  return null;
+};
